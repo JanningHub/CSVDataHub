fc7d5459ced70b7414b07c29f34ff7e19362599d
janning-owns-it <lucasjanning.works@gmail.com>
2026-05-15 02:52:32 +0100

    Initial commit

---
+++ src/main/java/com/jann/csv_data_hub/CsvDataHubApplication.java
@@ -0,0 +1,13 @@
+package com.jann.csv_data_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvDataHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvDataHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jann/csv_data_hub/CsvDataHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jann.csv_data_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvDataHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
